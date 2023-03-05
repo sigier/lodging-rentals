3fa18f3828324bdb18d8c2545cf590a54d0ab69a
sigier <jotulw@proton.me>
2023-03-05 20:42:57 +0200

    Spinner component added

--- src/utility/Spinner/Spinner.js
+++ src/utility/Spinner/Spinner.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Spinner.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core'
+library.add(faSpinner);
 
 const Spinner = () => {
 
     return(
-        <h1>Spinner</h1>
+        <div className="spinner-wrapper">
+                <FontAwesomeIcon icon="spinner" size="6x" spin/>
+        </div>
     );
 }
 
