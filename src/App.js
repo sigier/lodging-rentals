cac67620d091895c70cd597de51dc6dc56c93130
sigier <jotulw@proton.me>
2023-03-13 15:24:10 +0200

    "Suspense " function added to App-file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import {Switch, Route }  from 'react-router-dom';
 import NavBar from './utility/NavBar/NavBar';
-import Home from './pages/Home/Home';
-import FullVenue from './pages/FullVenue/FullVenue';
-import { BrowserRouter } from 'react-router-dom';
+import  Home from './pages/Home/Home';
+ import { BrowserRouter } from 'react-router-dom';
 import Modal from './utility/Modal/Modal';
+import Spinner from './utility/Spinner/Spinner';
 
+const FullVenue  = lazy(() => import('./pages/FullVenue/FullVenue'));
 
 
 const App = () => (
   <BrowserRouter>
+   <Suspense fallback={Spinner}>
      <NavBar/>
-     <Modal/>
+     <Modal/>    
      <Switch>
         <Route exact path="/" component={Home} ></Route>
         <Route exact path="/venue/:id" component={FullVenue}></Route>
       </Switch>
+    </Suspense>
   </BrowserRouter>
 
 )
