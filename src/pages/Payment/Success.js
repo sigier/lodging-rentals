8e4b1b4d6b914d5b0a45214b175078e2aa6395fc
sigier <jotulw@proton.me>
2023-03-28 09:23:36 +0300

    Payment scuccess page mark-up completed

--- src/pages/Payment/Success.js
+++ src/pages/Payment/Success.js
@@ -7,6 +7,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 library.add(faLongArrowAltRight);
 import api from '../../api';
 import Spinner from '../../utility/Spinner/Spinner';
+import {Link} from 'react-router-dom';
 
 
 const PaymentSuccess = (props) => {
@@ -20,8 +21,8 @@ const PaymentSuccess = (props) => {
         const token = props.auth.token;
         const data = {stripeToken, token};
         api.completePayment(data).then((res) =>{
-            setUserData(res.data.reservationDetails);
-            setReservation(res.data.userData);
+            setReservation(res.data.reservationDetails);
+            setUserData(res.data.userData);
             setLoading(false);
         });
    
@@ -32,18 +33,63 @@ const PaymentSuccess = (props) => {
         return(<Spinner/>);
     }
     
-
+    const rd = reservation;   
+    const vd = rd.venueData;
+    
     return(
-        <div className='reservation-success row'>
-            <h1 className='col m12 center'>Start packing!</h1>
-            <div className='resv-details col s8 offset-s2'>
-                <div className='confirmed col m12 row'>
-                    <div>
-                        <FontAwesomeIcon icon="long-arrow-alt-right" size="1x" color='#ED0000'/>Confirmed:
+        <div className="reservation-success row">
+                <h1 className="col m12 center">Start and Pack!</h1>
+                <div className="resv-details col s8 offset-s2">
+                    <div className="confirmed col m12 row">
+                        <FontAwesomeIcon icon="long-arrow-alt-right" size="1x" color="#ED0000" />Confirmed: {rd.diffDays} nights in {vd.location}         
+                        <div className="header-text">
+                            <div>Booked by: {props.auth.email}</div>
+                            <div>{moment().format('MMMM Do, YYYY')}</div>
+                        </div>
+                    </div>
+                    <div className="confirmed-detail row">
+                        <div className="col m5">
+                            <div className="bordered col">
+                                <div className="col m12 upper">
+                                    <div className="left">Check in</div><div className="right">Check out</div>
+                                </div>
+                                <div className="col m12 lower">
+                                    <div className="left">{moment(rd.checkIn).format('MMM Do, YYYY')}</div><div className="right">{moment(rd.checkOut).format('MMM Do, YYYY')}</div>
+                                </div>
+                                <div className="col m12 title-text">
+                                    {vd.title}
+                                </div>  
+                                <div className="col m12 details">
+                                    {vd.details}
+                                </div>  
+                            </div>
+                        </div>
+
+
+                        <div className="col m7">
+                            <div className="bordered col">
+                                <div className="col m12 upper charges">
+                                    <div className="charges-text col m12">Charges</div>
+                                    <div className="row col m12">
+                                        <div className="left">${rd.pricePerNight} x {rd.diffDays} days</div>
+                                        <div className="right">${rd.totalPrice}</div>
+                                    </div>
+                                    <div className="row col m12">
+                                        <div className="left">Discount</div>
+                                        <div className="right">$0</div>
+                                    </div>                                
+                                    <div className="row col m12 total">
+                                        <div className="left">TOTAL</div>
+                                        <div className="right">${rd.totalPrice}</div>
+                                    </div>
+                                </div>
+                                <div className="col m12 row">To modify your reservation in the future, visit the link <Link to="/account">account page</Link>.</div>
+                                <div className="col m12 resv-image"><img src={vd.imageUrl} /></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     )
 };
 
