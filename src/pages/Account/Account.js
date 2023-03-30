be4dcca7cec0e02159457da6dd41533f87201349
sigier <jotulw@proton.me>
2023-03-30 10:18:17 +0300

    Account page routes added

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -4,12 +4,23 @@ import { connect } from 'react-redux'
 import Booking from './Booking';
 import AccountSidebar from './AccountSideBar';
 import ChangePassword from './ChangePassword';
+import { Route } from 'react-router-dom';
 
 
 const Account = () =>{
     return (
         <div className='account container-fluid'>
             <AccountSidebar/>
+            <div className='row'>
+                <div className='col s8 offset-s3'>
+                    <Route exact path="/account" render={()=>
+                        <h1>Choose component</h1>
+                    }/>
+                    <Route exact path="/account/reservations/confirmed" component={Booking}/>
+                    <Route exact path="/account/reservations/past" component={Booking}/>
+                    <Route exact path="/account/change-pass" component={ChangePassword}/>
+                </div>
+            </div>
         </div>
     )
 }
